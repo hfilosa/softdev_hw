dc21729591ef463f97ab0f2d016d7ecab0c7cb34
Henry Filosa <hfilosa@gmail.com>
2016-04-01 09:27:24 -0400

    asd

--- hw06/strength.py
+++ hw06/strength.py
@@ -3,10 +3,11 @@ NUMERALS="1234567890"
 CHARACTERS=".?!&#,;:-_*"
 
 def pass_strength( p ):
-    ans = [1 if x in UC_LETTERS else 2 if x in NUMERALS else 3 if x in CHARACTERS else 0 for x in p]
+    ans = [5 if x in UC_LETTERS else 7 if x in NUMERALS else 10 if x in CHARACTERS else 2 for x in p]
     num=0
-    for (x in range(len(ans))):
-        num+=ans[x]
-    num/len(ans)
+    print ans
+    for x in ans:
+        num+=x
+    return num/len(ans) 
 
-print pass_strength("noobpass")
+print pass_strength("L3ATPass!")
